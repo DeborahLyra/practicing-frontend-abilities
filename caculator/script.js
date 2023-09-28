0525a043ab09a9859154383bb2373ee86d8b99d8
Deb student <142942076+DebStudent@users.noreply.github.com>
2023-09-27 21:58:01 -0300

    adicionando função do botão deletar

--- caculator/script.js
+++ caculator/script.js
@@ -1,15 +1,16 @@
 const btns = document.querySelectorAll('.btn')
 const input = document.querySelector('input')
-const result = document.querySelector('.result')
+const result = document.querySelector('#result')
 const clean = document.querySelector('#clean')
+const del = document.querySelector('#delete')
 
 
 let operation = ''
 
 btns.forEach(btn =>{
     btn.addEventListener('click', () =>{
-        if(btn.innerHTML !== '=' && btn.innerHTML !== 'ac' ){
-            operation += btn.innerHTML
+        if(btn.value !== '=' && btn.value !== 'ac' && btn.value !== 'delete' ){
+            operation += btn.value
             input.value = operation
         }       
     })
@@ -19,6 +20,7 @@ result.addEventListener('click', ()=>{
     if (operation){
         const valueResult = eval(operation)
         input.value = valueResult
+        operation = valueResult
    } else{
        input.value = operation
    }
@@ -27,4 +29,12 @@ result.addEventListener('click', ()=>{
 clean.addEventListener('click', ()=>{
     operation = ''
     input.value = ''
+})
+
+del.addEventListener('click', () =>{
+    if(input.value.length > 0){
+        operation = operation.slice(0, -1);
+        input.value = operation;
+    }
+   
 })
