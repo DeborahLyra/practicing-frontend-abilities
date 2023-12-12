43c671ba27a259217b4127f324db1c2f657fbd8d
Deborah Lyra <deborahp.lyra@gmail.com>
2023-12-12 16:02:15 -0300

    projeto com  alguns comentarios

--- web-service/src/app/page.tsx
+++ web-service/src/app/page.tsx
@@ -1,36 +1,73 @@
 "use client";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react" 
 import { api } from "../../services/api";
 
-const inicialList = [
-  { id: 0, titulo: "Emma"}
-]
+interface Livros { //aqui cira uma interface que vai servir de base para armazenar a lista
+  id: number,
+  titulo: string
+}
 
 export default function Home() {
-  const [load, setLoad] = useState([])
-  const [items, setItems] = useState(inicialList)
+  const [load, setLoad] = useState(false)
+  const [textInput, setTextInput] = useState("")
+  const [items, setItems] = useState<Livros[]>([])
 
-  interface listaOriginal {
-    titulo: "",
-    autor: ""
+ 
+
+  async function addItemLista(){ //essa função adiciona um item na lista
+    const data = {titulo: textInput}
+
+    try{
+      const response = await api.post("/livros", data)
+      console.log(response)
+      loadLista()
+    } catch(e){
+      console.log("erro")
+    }
   }
 
-  async function getLista() {
-    const response = await api.get("/livros")
+  async function loadLista(){  //essa função é a responsável por carregar a página, é ela que 
+                              //faz com que a medida que os itens forem adicionados, vão aparecer simultaneamente
+    setLoad(true)
+    try {
+      const response = await api.get("/livros")
     setItems(response.data)
     console.log(items)
     console.log(response.data)
+    } catch (error) {
+      console.log("erro")
+    } finally{
+      setLoad(false)
+    }
   }
 
+  useEffect(()=> {  //aqui usa o useEffect para a lista aparecer assim que carrega a página
+    loadLista()
+  }, [])
+
+ async function deleteItem(id: number){ //essa a função responsável por deletar o item da lista
+    await api.delete(`/livros/${id}`) 
+    loadLista()
+  }
 
   return (
+    //logo abaixo é o input, nele chama a funcão setTextInput quando há mudanças nele, que armazena o valor escrito nele
+    //depois tem o botão que chama a função addItemLista no onClick
+    //depois vem a lisa, onde usa o map para adicionar diversos itens em cada li
     <>
-      <input type="text" placeholder="digite um título aqui"/> <br/><br/>
+    
+    <h1>Lista de livros</h1>
+      <input onChange={(e) => setTextInput(e.target.value)} type="text" placeholder="digite um título aqui"/> <br/><br/>
       
-      <button onClick={() => getLista()}>clique para adicionar mais </button>
+      <button onClick={addItemLista}>clique para adicionar</button>
+      <span>{load && "carregando"}</span>
       <ul>
         {items.map((item, index) => (
-          <li key={index}>{item.titulo}</li>
+          <li key={index}>
+            {item.titulo}
+            <button onClick={()=>{deleteItem(item.id)}}>deletar</button>
+            </li>
+          
         ))}
       </ul>
 
