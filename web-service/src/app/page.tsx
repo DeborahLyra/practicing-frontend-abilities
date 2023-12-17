2d3b1fabd8a6792bc557895edc5a037f55117366
Deborah Lyra <deborahp.lyra@gmail.com>
2023-12-17 14:04:04 -0300

    corrigindo

--- web-service/src/app/page.tsx
+++ web-service/src/app/page.tsx
@@ -1,66 +1,70 @@
 "use client";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { api } from "../../services/api";
 
-
-interface Livros { //aqui cira uma interface que vai servir de base para armazenar a lista
-  id: number,
-  titulo: string
-  isEditing: boolean,
+interface Livros {
+  //aqui cira uma interface que vai servir de base para armazenar a lista
+  id: number;
+  titulo: string;
+  isEditing: boolean;
 }
 
 export default function Home() {
-  const [load, setLoad] = useState(false)
-  const [textInput, setTextInput] = useState("")
-  const [items, setItems] = useState<Livros[]>([])
+  const [load, setLoad] = useState(false);
+  const [textInput, setTextInput] = useState("");
+  const [items, setItems] = useState<Livros[]>([]);
 
-
-
-  async function addItemLista() { //essa função adiciona um item na lista
-    const data = { titulo: textInput }
+  async function addItemLista() {
+    //essa função adiciona um item na lista
+    const data: Omit<Livros, "id"> = { titulo: textInput, isEditing: false };
 
     try {
-      const response = await api.post("/livros", data)
-      console.log(response)
-      loadLista()
+      const response = await api.post("/livros", data);
+      console.log(response);
+      loadLista();
     } catch (e) {
-      console.log("erro")
+      console.log("erro");
     }
   }
 
-  useEffect(() => {  //aqui usa o useEffect para a lista aparecer assim que carrega a página
-    loadLista()
-  }, [])
-
-  async function loadLista() {  //essa função é a responsável por carregar a página, é ela que 
+  async function loadLista() {
+    //essa função é a responsável por carregar a página, é ela que
     //faz com que a medida que os itens forem adicionados, vão aparecer simultaneamente
-    setLoad(true)
+    setLoad(true);
     try {
-      const response = await api.get("/livros")
-      setItems(response.data)
-      console.log(response.data)
+      const response = await api.get("/livros");
+      setItems(response.data);
+      console.log(items);
+      console.log(response.data);
     } catch (error) {
-      console.log("erro")
+      console.log("erro");
     } finally {
-      setLoad(false)
+      setLoad(false);
     }
   }
 
+  useEffect(() => {
+    //aqui usa o useEffect para a lista aparecer assim que carrega a página
+    loadLista();
+  }, []);
 
-  async function HandleDeleteItem(id: number) { //essa a função responsável por deletar o item da lista
+  async function deleteItem(id: number) {
+    //essa a função responsável por deletar o item da lista
     try {
-      await api.delete(`/livros/${id}`)
-      const filteredItems = items.filter((item) => item.id !== id)
-      setItems(filteredItems)
-    } catch (error) {
-      console.log("erro")
+      await api.delete(`/livros/${id}`);
+      const filterItems = items.filter((item) => {
+        item.id !== id;
+      });
+      setItems(filterItems);
+      loadLista();
+    } catch (e) {
+      console.log("erro");
     }
-
   }
 
-  async function handleEditItem(id: number) {
+  function editarItem(id: number) {
     const changedItems = items.map((item) => {
-      if (item.id === id) {
+      if (item.id == id) {
         return { ...item, isEditing: true };
       }
       return item;
@@ -69,10 +73,9 @@ export default function Home() {
     setItems(changedItems);
   }
 
-  function handleUpdateItem(id: number) {
-
+  function updateItem(id: number) {
     const changedItems = items.map((item) => {
-      if (item.id === id) {
+      if (item.id == id) {
         return { ...item, isEditing: false };
       }
       return item;
@@ -81,10 +84,10 @@ export default function Home() {
     setItems(changedItems);
   }
 
-  function handleChangeItem(id: number, textValue: string) {
+  function onChangeItem(id: number, text: string) {
     const changedItems = items.map((item) => {
-      if (item.id === id) {
-        return { ...item, nome: textValue };
+      if (item.id == id) {
+        return { ...item, titulo: text };
       }
       return item;
     });
@@ -92,46 +95,63 @@ export default function Home() {
     setItems(changedItems);
   }
 
-
   return (
     //logo abaixo é o input, nele chama a funcão setTextInput quando há mudanças nele, que armazena o valor escrito nele
     //depois tem o botão que chama a função addItemLista no onClick
     //depois vem a lisa, onde usa o map para adicionar diversos itens em cada li
     <>
       <h1>Lista de livros</h1>
-      <div style={{ marginBottom: 10 }}>
-        <input
-          onChange={(e) => setTextInput(e.target.value)}
-          placeholder="Digite o seu texto aqui..."
-        />
-        <button onClick={addItemLista}>Enviar</button>
-      </div>
-
-      <span>{load && "Carregando..."}</span>
-
+      <input
+        onChange={(e) => setTextInput(e.target.value)}
+        type="text"
+        placeholder="digite um título aqui"
+      />
+      <br />
+      <br />
+      <button onClick={addItemLista}>clique para adicionar</button>
+      <span>{load && "carregando"}</span>
       <ul>
-        {items.map((item) => (
-          <li key={item.id}>
+        {items.map((item, index) => (
+          <li key={index}>
             {item.isEditing ? (
               <input
-                onChange={(e) => handleChangeItem(item.id, e.target.value)}
                 value={item.titulo}
+                placeholder="digite"
+                onChange={(e) => {
+                  onChangeItem(item.id, e.target.value);
+                }}
               />
             ) : (
               item.titulo
             )}
-
-            {item.isEditing ? (
-              <button onClick={() => handleUpdateItem(item.id)}>Save</button>
+            {item.isEditing == true ? (
+              <button
+                onClick={() => {
+                  updateItem(item.id);
+                }}
+              >
+                salvar
+              </button>
             ) : (
-              <button onClick={() => handleEditItem(item.id)}>Edit</button>
+              <button
+                onClick={() => {
+                  editarItem(item.id);
+                }}
+              >
+                editar
+              </button>
             )}
 
-            <button onClick={() => HandleDeleteItem(item.id)}>Delete</button>
+            <button
+              onClick={() => {
+                deleteItem(item.id);
+              }}
+            >
+              deletar
+            </button>
           </li>
         ))}
       </ul>
-
     </>
-  )
+  );
 }
